f0e5e67855d83bdbc16c09641317bff952b67a04
George Blackmore <gtblackmore@gmail.com>
2019-12-11 20:42:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,21 @@
 #write your code here
 
 def countdown(integer)
-  
+  while integer > 0
+      puts "#{integer} SECONDS"
+      integer -= 1 
+    end
+    if integer == 0
+      return "Happy New Year"
+end
+
+def countdown_with_sleep(integer)
+    while integer > 0
+      puts "#{integer} SECONDS"
+      integer -= 1 
+      sleep(1)
+    end
+    if integer == 0
+      return "Happy New Year"
+    end
 end
